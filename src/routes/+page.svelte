985d6be34939937f6334f2cd0620f64c9952093a
calcalayy <minelandsmc14@gmail.com>
2023-09-23 20:58:02 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Card from './card.svelte';
+</script>
+
+<div class="flex flex-col justify-center text-center">
+    <h1 class="text-2xl font-bold text-primary p-2">zumo.cat</h1>
+    <div class="flex justify-center p-4">
+        <Card/>
+    </div>
+</div>
---
+++ src/routes/card.svelte
@@ -0,0 +1,6 @@
+<div class="artboard phone-1 artboard-demo bg-secondary-content">
+    <div class="flex flex-col">
+        <img src="/cat.png" class="rounded-t-lg" alt="zumocat">
+        <div class="py-20"></div>
+    </div>
+</div>
