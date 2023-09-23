75e08a6dae6a2f7aa6b367f49df18764660f34da
calcalayy <minelandsmc14@gmail.com>
2023-09-23 22:38:23 +0100

    improved suggestions

--- src/routes/card.svelte
+++ src/routes/card.svelte
@@ -1,7 +1,15 @@
+<script>
+    import Tag from './hash.svelte';
+</script>
+
 <div class="phone-1 bg-secondary-content rounded-2xl w-3/5  max-h-sm">
     <div class="flex flex-col">
         <img src="/cat.png" class="rounded-t-lg align-top" alt="zumocat">
         <h1 class="font-bold text-2xl align-left mr-auto p-2">cat</h1>
-        <p class="align-left text-left pl-2 pb-6 text-neutral-content">#cute #paper</p>
+        <div class="flex pl-2 pb-2">
+            <Tag name="cute"></Tag>
+            <p class="pl-1"/>
+            <Tag name="eating"></Tag>
+        </div>
     </div>
 </div>
---
+++ src/routes/hash.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let name;
+</script>
+
+<div class="rounded-full bg-blue-600 bg-opacity-20">
+    <p class="text-blue-300 font-bold phone-1 px-2">{name}</p>
+</div>
