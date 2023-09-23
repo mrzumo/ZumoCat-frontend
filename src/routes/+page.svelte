d7fa146cbeed4281ef141fe0f357bda229c3a8d8
calcalayy <minelandsmc14@gmail.com>
2023-09-23 21:41:48 +0100

    grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,9 @@
 
 <div class="flex flex-col justify-center text-center">
     <h1 class="text-2xl font-bold text-primary p-2">zumo.cat</h1>
-    <div class="flex justify-center p-4">
-        <Card/>
+    <div class="container m-auto grid justify-items-center gap-8 grid-cols-1 md:grid-cols-2">
+            <Card></Card>
+            <Card></Card>
     </div>
 </div>
 
--- src/routes/card.svelte
+++ src/routes/card.svelte
@@ -1,7 +1,7 @@
-<div class="artboard phone-1 bg-secondary-content rounded-2xl">
+<div class="phone-1 bg-secondary-content rounded-2xl w-3/5  max-h-sm">
     <div class="flex flex-col">
         <img src="/cat.png" class="rounded-t-lg align-top" alt="zumocat">
         <h1 class="font-bold text-2xl align-left mr-auto p-2">cat</h1>
-        <p class="align-left text-left pl-2 text-neutral-content">#cute #paper</p>
+        <p class="align-left text-left pl-2 pb-6 text-neutral-content">#cute #paper</p>
     </div>
 </div>
