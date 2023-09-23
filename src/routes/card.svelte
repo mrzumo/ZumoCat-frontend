2fa70b2655ec7cfbd586b49622ce8a5121cf9243
calcalayy <minelandsmc14@gmail.com>
2023-09-23 21:22:51 +0100

    placeholder done

--- src/routes/card.svelte
+++ src/routes/card.svelte
@@ -1,6 +1,7 @@
-<div class="artboard phone-1 artboard-demo bg-secondary-content">
+<div class="artboard phone-1 bg-secondary-content rounded-2xl">
     <div class="flex flex-col">
-        <img src="/cat.png" class="rounded-t-lg" alt="zumocat">
-        <div class="py-20"></div>
+        <img src="/cat.png" class="rounded-t-lg align-top" alt="zumocat">
+        <h1 class="font-bold text-2xl align-left mr-auto p-2">cat</h1>
+        <p class="align-left text-left pl-2 text-neutral-content">#cute #paper</p>
     </div>
 </div>
