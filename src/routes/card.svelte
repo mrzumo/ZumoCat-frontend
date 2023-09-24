5843cdb0afdcaa01dd3a7a576b3d82ffddf33889
calcalayy <minelandsmc14@gmail.com>
2023-09-24 14:32:46 +0100

    fix image

--- src/routes/card.svelte
+++ src/routes/card.svelte
@@ -2,9 +2,9 @@
     import Tag from './hash.svelte';
 </script>
 
-<div class="phone-1 bg-secondary-content rounded-2xl w-3/5  max-h-sm">
+<div class="phone-1 bg-secondary-content rounded-2xl w-3/5 max-h-sm">
     <div class="flex flex-col">
-        <img src="/cat.png" class="rounded-t-lg align-top" alt="zumocat">
+        <img src="/cat.png" class="rounded-t-lg align-top pointer-events-none" alt="zumocat">
         <h1 class="font-bold text-2xl align-left mr-auto p-2">cat</h1>
         <div class="flex pl-2 pb-2">
             <Tag name="cute"></Tag>
