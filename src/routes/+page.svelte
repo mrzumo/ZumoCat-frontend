8ebd249e20c06a4dd16b2d3415ad085d7cad7b86
calcalayy <minelandsmc14@gmail.com>
2023-09-24 14:06:31 +0100

    pushhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,16 @@
     import Card from './card.svelte';
 </script>
 
-<div class="flex flex-col justify-center text-center">
+<div class="flex flex-col justify-center text-center min-h-screen">
     <h1 class="text-2xl font-bold text-primary p-2">zumo.cat</h1>
     <div class="container m-auto grid justify-items-center gap-8 grid-cols-1 md:grid-cols-2">
             <Card></Card>
             <Card></Card>
     </div>
+    <div class="py-4"/>
+    <footer class="footer footer-center p-4 bg-secondary-content text-base-content">
+        <aside>
+            <a href="/credits" class="font-bold text-xl">credits</a>
+        </aside>
+    </footer>
 </div>
-
-<!-- SAHDHASHSHSDGGDG -->
