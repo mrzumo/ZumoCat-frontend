5585f6ff799072e10a265c87ac1ee0aeef90de81
Zumodenaranja <55620434+Zumodenaranja@users.noreply.github.com>
2023-10-18 19:13:19 +0200

    avoid lawsuit from gigi

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,5 +1,5 @@
 <div class="mb-auto mt-auto flex min-h-screen flex-col justify-center text-center">
   <h1 class="p-2 text-2xl font-bold text-primary">zumo.cat</h1>
-  <p class="text-secondary">frontend made by yours truly calcium <br /> sw1ndler made the backend <br /> sexy rofl dofl on the discord bot <br /> and zumo for giving cat pics</p>
+  <p class="text-secondary">frontend made by yours truly calcium <br /> sw1ndler made the backend <br /> sexy rofl dofl on the discord bot <br /> and zumo for giving cat pics <br /> also Gigi for making the feed work and sum other stuff</p>
   <a class="ml-auto mr-auto justify-center p-2 text-2xl font-bold text-primary" href="/">back</a>
 </div>
